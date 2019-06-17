f520785e6c285e914c8e5ecc3e09f34416ed830f
Manan Gakhar <mggakhar@amazon.com>
2019-06-18 03:23:27 +0530

    data visualization

--- iris-project/iris-ml.py
+++ iris-project/iris-ml.py
@@ -37,3 +37,17 @@ print(dataset.groupby('class').size())
 for key, item in dataset.groupby('class'):
     print(dataset.groupby('class').get_group(key))
 
+# box and whisker plots - box boundary at Q1 and Q3 quartiles
+# while the middle line in box is Q2 quartile (median)
+dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+plt.show()
+
+# histogram - a diagram consisting of rectangles
+# whose area is proportional to the frequency of a variable
+# and whose width is equal to the class interval.
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
